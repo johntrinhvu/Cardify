e05cfa52cb41872190e4bf2590002385d1d66d90
John Vu <johntrinhvu@yahoo.com>
2024-04-20 04:13:06 -0700

    do this portion of navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 import CardLogo from '../../assets/CardLogo.png';
+import { CgProfile } from "react-icons/cg";
 import './NavBar.css';
 
 export default function NavBar({ user, setUser }) {
+  const [showDropdown, setShowDropdown] = useState(false);
+
   function handleLogOut() {
     userService.logOut();
     setUser(null);
+  }
+
+  function toggleDropdown() {
+    setShowDropdown(prevState => !prevState);
+  }
+
+  function handleLinkClick() {
+    setShowDropdown(false);
   }
 
   return (
@@ -15,9 +27,18 @@ export default function NavBar({ user, setUser }) {
             <Link to="/">
                 <img src={ CardLogo } alt="CardLogo" className="navbar-logo" />
             </Link>
-            <div className="navbar-right">
-                <p className="navbar-user">Welcome, {user.name}</p>
-                <Link to="" onClick={handleLogOut} className="navbar-link">Log Out</Link>
+            <div className="dropdown" onClick={toggleDropdown}>
+                <div className="navbar-right">
+                    <div className="navbar-user">
+                        <CgProfile className="user-name" />&nbsp;<p className="user-name">{user.name}</p>
+                    </div>
+                    {showDropdown && (
+                        <div className="dropdown-content">
+                            <Link to="/cards/:Id" className="dropdown-item" onClick={handleLinkClick}>View Cards</Link>
+                            <Link to="" onClick={handleLogOut} className="dropdown-item">Log Out</Link>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     </nav>
