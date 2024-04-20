6e9e97c377a44c04972549625ed357e8c0f0b349
John Vu <johntrinhvu@yahoo.com>
2024-04-20 05:07:57 -0700

    create controller, model, and route for card

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,7 @@ export default function NewOrderPage() {
     return (
         <div>
             <h1>Home Page!</h1>
-            <Link to="/card/new" className="navbar-link">BizCard</Link>
+            <Link to="/cards/new" className="navbar-link">BizCard</Link>
         </div>
     );
   }
---
+++ src/utilities/cards-api.js
@@ -0,0 +1,34 @@
+import sendRequest from "./send-request";
+const BASE_URL = '/api/cards';
+
+export async function createCard(cardData) {
+  return sendRequest(`${BASE_URL}/new`, 'POST', cardData, true);
+}
+
+export async function fetchCardById(cardId) {
+  return sendRequest(`${BASE_URL}/${cardId}`, 'GET');
+}
+
+export async function updateCard(updatedCard) {
+  try {
+    const updated = sendRequest(`${BASE_URL}/${updatedCard.id}`, 'PUT', updatedCard, true);
+    return updated;
+    
+  } catch (error) {
+    throw new Error('Failed to update the card');
+  }
+}
+
+export async function deleteCard(cardId) {
+  return sendRequest(`${BASE_URL}/${cardId}`, 'DELETE');
+}
+
+export async function fetchCards() {
+  try {
+    const cards = await sendRequest(BASE_URL, 'GET');
+    return cards;
+    
+  } catch (error) {
+    throw new Error('Failed to fetch cards');
+  }
+}
