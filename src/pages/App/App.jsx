1865f305caf1b49e89f991e24d71abb97cb61834
John Vu <johntrinhvu@yahoo.com>
2024-04-19 21:37:40 -0700

    implement mongo into server

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,31 +1,11 @@
-import logo from '../../logo.svg';
 import '../App/App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          wake up
-        </p>
-        <p>
-          penis
-        </p>
-      </header>
-    </div>
+    <main className="App">
+      <p>
+        Hello World
+      </p>
+    </main>
   );
 }
-
-export default App;
