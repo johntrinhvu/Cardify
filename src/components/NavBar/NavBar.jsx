a4c2f0ca927e9d4c574e4bd8a82c403fb9e48d69
Jeff <jluo3002@gmail.com>
2024-04-20 01:16:30 -0700

    fixed biztoken

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,7 +12,7 @@ export default function NavBar({ user, setUser }) {
       <Link to="/orders">Order History</Link>
       &nbsp; | &nbsp;
       <Link to="/orders/new">New Order</Link>
-      &nbsp;&nbsp;
+      &nbsp; | &nbsp;
       <Link to="/Create">BizCard</Link>
       &nbsp;&nbsp;
       <span>Welcome, {user.name}</span>
