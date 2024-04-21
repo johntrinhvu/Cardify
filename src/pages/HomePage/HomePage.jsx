a0bd0ba4488e9684073197f643c4af92a1f3b472
John Vu <johntrinhvu@yahoo.com>
2024-04-21 00:06:34 -0700

    add some cool typing

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,8 +4,30 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Typography from '@mui/material/Typography';
+import Typewriter from 'typewriter-effect/dist/core';
 
 export default function HomePage() {
+    React.useEffect(() => {
+        const typewriter = new Typewriter('#typewriter', {
+            strings: [
+                'Welcome to Cardify.',
+                'Create your own virtual business card.'
+            ],
+            autoStart: true,
+            loop: true,
+        });
+
+        typewriter
+            .pauseFor(2500)
+            .changeDeleteSpeed(200)
+            .changeDelay(50)
+            .start()
+
+        return () => {
+            typewriter.stop();
+        };
+    }, []);
+
     return (
         <Grid container 
             spacing={3} 
@@ -14,7 +36,7 @@ export default function HomePage() {
             style={{ justifyContent: 'center', minHeight: '80vh' }}>
             <Grid item xs={12}>
                 <Typography variant="h3">
-                    Welcome to Cardify.
+                    <span id="typewriter"></span>
                 </Typography>
             </Grid>
             <Grid item xs={12}>
